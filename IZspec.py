2fec3b643c50ddfa11c563dde1fb5639533f5c6f
slodziu <somlgpl@gmail.com>
2024-10-11 11:06:46 +0100

    added new files to IZ spectra

--- IZspec.py
+++ IZspec.py
@@ -22,23 +22,26 @@ for filename in os.listdir(directory):
         I_values.extend(data.iloc[1].tolist())
 
 # Convert lists to numpy arrays for fitting
-Z_values = np.array(Z_values)
-I_values = np.array(I_values)
+Z_values = np.array(Z_values)*1e8
+I_values = np.array(I_values)*1e9
+log_Z_vals = [np.log(x) for x in Z_values]
+log_I_values = [np.log(x) for x in I_values]
+
 print(len(Z_values))
 # Define the exponential model function
 def exponential_model(Z, a, b, c):
-    return a * np.exp(b * Z) + c
+    return a * np.exp(-b * Z) + c
+
 
 # Fit the exponential model to the data with more lenient bounds
-params, covariance = curve_fit(exponential_model, Z_values, I_values, bounds=(-np.inf, np.inf))
+params, covariance = curve_fit(exponential_model, Z_values, I_values, maxfev=10000, p0=[0.26, 8, 0])
 
 # Generate fitted I values using the model
-I_fitted = exponential_model(Z_values, *params)
+
 print('Fit parameters', params)
 # Plot the data
 plt.figure(figsize=(10, 6))
-plt.plot(Z_values, I_values, 'o', label='I vs Z')
-plt.plot(Z_values, I_fitted, '-', label='Fitted model')
+plt.plot(log_Z_vals, log_I_values, 'o', label='Data')
 plt.xlabel('Tip Distance (m)')
 plt.ylabel('Tunneling Current (A)')
 plt.title('I vs Z Spectrum with Exponential Fit')
