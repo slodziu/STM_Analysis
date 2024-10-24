39a2016b71f8dd56eabe11011f1cae307d8dde5f
slodziu <somlgpl@gmail.com>
2024-10-24 12:43:44 +0100

    Added a 3D plot to FFT sim and fixed coefficients

--- FFTSim.py
+++ FFTSim.py
@@ -5,42 +5,78 @@ import matplotlib.pyplot as plt
 a = 0.246  # lattice constant for graphene in nm
 k = 2 * np.pi / a  # wave vector
 n = 1000  # size of the simulation grid
-real_space_size =2 #nm
+real_space_size = 1 # nm
 theta = np.pi / 3  
-kx1, ky1 = k, 0
-kx2, ky2 = k * np.cos(theta), k * np.sin(theta)
-# Second harmonic wave vectors
-kx1_2, ky1_2 = 2 * kx1, 2 * ky1
-kx2_2, ky2_2 = 2 * kx2, 2 * ky2
 
-# Third harmonic wave vectors
-kx1_3, ky1_3 = 3 * kx1, 3 * ky1
-kx2_3, ky2_3 = 3 * kx2, 3 * ky2
+# Function to generate harmonic wave vectors
+def generate_wave_vectors(k, theta, n_harmonics):
+    wave_vectors = []
+    for n in range(1, n_harmonics + 1):
+        kx1, ky1 = n * k, 0
+        kx2, ky2 = n * k * np.cos(theta), n * k * np.sin(theta)
+        wave_vectors.append((kx1, ky1, kx2, ky2))
+    return wave_vectors
+
+# Generate wave vectors for the first harmonics
+n_harmonics = 5
+wave_vectors = generate_wave_vectors(k, theta, n_harmonics)
+P_n = [1, 0.1, 0.1, 0.1, 0.1]
+
 # Generate real space lattice points
 x = np.linspace(-real_space_size/2, real_space_size/2, n)
 y = np.linspace(-real_space_size/2, real_space_size/2, n)
 X, Y = np.meshgrid(x, y)
-P_1,P_2 = 1, 0.5
-lattice_real_space = np.cos(kx1 * X + ky1 * Y) + np.cos(kx2 * X + ky2 * Y) +0.5 * (np.cos(kx1_2 * X + ky1_2 * Y) + np.cos(kx2_2 * X + ky2_2 * Y)) +  0.25 * (np.cos(kx1_3 * X + ky1_3 * Y) + np.cos(kx2_3 * X + ky2_3 * Y)) 
-sigma = 50  # standard deviation for Gaussian smoothing
+lattice_real_space = np.zeros_like(X)
+
+# Adding contributions from A and B sites
+for i, (kx1, ky1, kx2, ky2) in enumerate(wave_vectors):
+    P = P_n[i]
+    # A-site contribution
+    lattice_real_space += P * (np.cos(kx1 * X + ky1 * Y) + np.cos(kx2 * X + ky2 * Y))
+    # B-site contribution with a phase shift (e.g., pi)
+    lattice_real_space += P * (np.cos(kx1 * X + ky1 * Y + np.pi/3) + np.cos(kx2 * X + ky2 * Y + np.pi/3))
+
+sigma = 10000  # standard deviation for Gaussian smoothing
 gaussian_envelope = np.exp(-(X**2 + Y**2) / (2 * sigma**2))
-lattice_real_space_smooth = lattice_real_space * gaussian_envelope
-print(lattice_real_space_smooth)
+lattice_real_space_smooth = lattice_real_space
+
 # Plot real space lattice
-plt.title("HOPG Lattice in Real Space")
-plt.imshow(lattice_real_space, cmap='plasma')
+plt.title("HOPG Lattice in Real Space (A and B Sites)")
+plt.imshow(lattice_real_space_smooth, cmap='plasma')
 plt.colorbar()
-plt.savefig("Produced_Plots/FFTSIM/HOPG_Lattice_Real_Space.png",dpi=300)
+plt.xticks(ticks=np.linspace(0, n, 5), labels=np.round(np.linspace(-real_space_size/2, real_space_size/2, 5), 2))
+plt.yticks(ticks=np.linspace(0, n, 5), labels=np.round(np.linspace(-real_space_size/2, real_space_size/2, 5), 2))
+plt.xlabel("X (nm)")
+plt.ylabel("Y (nm)")
+plt.savefig("Produced_Plots/FFTSIM/HOPG_Lattice_Real_Space.png", dpi=300)
 plt.show()
 
+# Plot 3D 'sideways' plot of the real space lattice
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, lattice_real_space_smooth, cmap='plasma')
 
+ax.set_title("3D Sideways Plot of HOPG Lattice in Real Space")
+ax.set_xlabel("X (nm)")
+ax.set_ylabel("Y (nm)")
+ax.set_zlabel("Amplitude")
+
+plt.savefig("Produced_Plots/FFTSIM/HOPG_Lattice_Real_Space_3D.png", dpi=300)
+plt.show()
 # Compute 2D FFT for reciprocal space
-lattice_reciprocal_space = np.fft.fftshift(np.fft.fft2(lattice_real_space))
+lattice_reciprocal_space = np.fft.fftshift(np.fft.fft2(lattice_real_space_smooth))
 magnitude_reciprocal = np.abs(lattice_reciprocal_space)
 
 # Plot reciprocal space (2D FFT result)
 plt.title("Reciprocal Space (2D FFT of HOPG Lattice)")
-plt.imshow(np.log(magnitude_reciprocal), cmap='plasma')
+plt.imshow(np.log(1 + magnitude_reciprocal), cmap='plasma')  # 1 + data for contrast
+kx = np.fft.fftshift(np.fft.fftfreq(n, d=(x[1] - x[0])))
+ky = np.fft.fftshift(np.fft.fftfreq(n, d=(y[1] - y[0])))
+plt.xticks(ticks=np.linspace(0, n, 5), labels=np.round(np.linspace(kx[0], kx[-1], 5), 2))
+plt.yticks(ticks=np.linspace(0, n, 5), labels=np.round(np.linspace(ky[0], ky[-1], 5), 2))
+plt.xlabel("kx (1/nm)")
+plt.ylabel("ky (1/nm)")
 plt.colorbar()
 plt.savefig("Produced_Plots/FFTSIM/HOPG_Lattice_Reciprocal_Space.png", dpi=300)
 plt.show()
+
