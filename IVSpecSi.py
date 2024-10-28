9aaa42c670b18d939fc6bbc65c193dbc66728415
slodziu <somlgpl@gmail.com>
2024-10-28 15:38:35 +0000

    Fixed formatting on IV spectra analysis for Si

--- IVSpecSi.py
+++ IVSpecSi.py
@@ -37,10 +37,10 @@ average_V = np.mean(all_V_values, axis=0)
 average_I = np.mean(all_I_values, axis=0)
 std_I = np.std(all_I_values, axis=0)
 
-# Plot the average I-V curve with error bars
+# Plot the average I-V curve with errors
 plt.figure(figsize=(10, 6))
-plt.plot(average_V, average_I, label='Average I-V Curve')
-plt.fill_between(average_V, average_I - std_I, average_I + std_I, color='b', alpha=0.2, label='Standard Deviation')
+plt.plot(average_V, average_I, label='Average I-V Curve',color='r')
+plt.fill_between(average_V, average_I - std_I, average_I + std_I, color='b', alpha=0.5, label='Standard Deviation')
 plt.xlabel('Tip Voltage (V)')
 plt.ylabel('Tip Current (A)')
 plt.title('Average I-V Curve with Error Bars')
@@ -69,12 +69,41 @@ print(f"Parameter errors: {errors}")
 
 # Generate the fitted curve
 fit = cubic_function(average_V, *params)
-plt.scatter(average_V, average_I, label=f'Averaged data from {file_count} spectra', s=10)  # 's=10' sets the size of the points
+plt.scatter(average_V, average_I, label=f'Averaged data from {file_count} spectra', s=2)  # 's=10' sets the size of the points
 plt.plot(average_V, fit, 'r-', label='Cubic Fit of the Data')  # 'r-' for red line
-plt.fill_between(average_V,fit - errors[0],fit + errors[0], color='r', alpha=0.8, label='Standard Deviation')
+plt.fill_between(average_V,fit - errors[0],fit + errors[0], color='r', alpha=0.3, label='Standard Deviation')
 plt.legend()
 plt.grid(True)
 plt.xlabel('Voltage (V)')
 plt.ylabel('Current (A)')
 plt.title('Average I vs V')
+plt.savefig('Produced_Plots/Silicon/IV_Si_Fit.png', dpi=300)
+plt.show()
+
+# Calculate the derivative of the cubic fit
+d_cubic_fit = np.polyder(params)
+
+# Evaluate the derivative at the V values
+dIdV = np.polyval(d_cubic_fit, average_V)
+
+# Calculate I/V
+I_over_V = average_I /average_V
+
+# Calculate (dI/dV)/(I/V)
+dIdV_over_IoverV = dIdV / I_over_V
+
+# Calculate the error on (dI/dV)/(I/V)
+dIdV_error = np.polyval(np.polyder(np.polyfit(average_V, std_I, 3)), average_V)
+I_over_V_error = std_I / average_V
+dIdV_over_IoverV_error = np.sqrt((dIdV_error / I_over_V)**2 + (dIdV * I_over_V_error / I_over_V**2)**2)
+
+# Plot (dI/dV)/(I/V) with error bars
+plt.plot(average_V, dIdV_over_IoverV, label='Derivative of fitted curve')  # 'ro' for red points only
+plt.fill_between(average_V, dIdV_over_IoverV - dIdV_over_IoverV_error, dIdV_over_IoverV + dIdV_over_IoverV_error, color='r', alpha=0.5, label='Error')
+plt.grid(True)
+plt.legend()
+plt.xlabel('Voltage (V)')
+plt.ylabel('(dI/dV)/(I/V)')
+plt.title('(dI/dV)/(I/V) vs V')
+plt.savefig('Produced_Plots/Silicon/IV_Si_dIdV_over_IoverV.png', dpi=300)
 plt.show()
