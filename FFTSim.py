b8142c44732db81199c55b362f0c69b1bd5fc2ba
slodziu <somlgpl@gmail.com>
2024-11-05 14:16:55 +0000

    Added A-B site simulation and the inverse space image

--- FFTSim.py
+++ FFTSim.py
@@ -8,7 +8,8 @@ def generate_wave_vectors(k, theta, n_harmonics):
     for n in range(1, n_harmonics + 1):
         kx1, ky1 = n * k, 0
         kx2, ky2 = n * k * np.cos(theta), n * k * np.sin(theta)
-        wave_vectors.append((kx1, ky1, kx2, ky2))
+        kx3, ky3 = n * k * np.cos(-theta), n * k * np.sin(-theta)
+        wave_vectors.append((kx1, ky1, kx2, ky2,kx3,ky3))
     return wave_vectors
 
 def generate_coefficients(num_coefficients,order=2):
@@ -28,16 +29,17 @@ def simulate_hopg_lattice(n_harmonics, k, theta, real_space_size,order,n):
     lattice_real_space = np.zeros_like(X)
 
     # Adding contributions from A and B sites
-    for i, (kx1, ky1, kx2, ky2) in enumerate(wave_vectors):
+    for i, (kx1, ky1, kx2, ky2, kx3, ky3) in enumerate(wave_vectors):
         P = P_n[i]
         # A-site contribution
-        lattice_real_space += P * (np.cos(kx1 * X + ky1 * Y) + np.cos(kx2 * X + ky2 * Y))
-        # B-site contribution with a phase shift
-        lattice_real_space += P * (np.cos(kx1 * X + ky1 * Y + np.pi/2) + np.cos(kx2 * X + ky2 * Y + np.pi/2))
+        lattice_real_space += P * (np.cos((kx1 * X + ky1 * Y)) + np.cos((kx2 * X + ky2 * Y))+np.cos((kx3 * X + ky3 * Y)))
 
-    sigma = 10  # standard deviation for Gaussian smoothing
+        # B-site contribution with a phase shift
+        lattice_real_space += P * (np.cos((kx1 * X + ky1 * Y) + np.pi) + np.cos((kx2 * X + ky2 * Y) + np.pi)+np.cos((kx3 * X + ky3 * Y) + np.pi))
+
+    sigma = 100  # standard deviation for Gaussian smoothing
     gaussian_envelope = np.exp(-(X**2 + Y**2) / (2 * sigma**2))
-    lattice_real_space_smooth = lattice_real_space
+    lattice_real_space_smooth = lattice_real_space 
 
     # Plot real space lattice
     plt.title("HOPG Lattice in Real Space (A and B Sites)")
@@ -74,10 +76,10 @@ def simulate_hopg_lattice(n_harmonics, k, theta, real_space_size,order,n):
 
     plt.xlabel(r"$k_x$ (1/nm)")
     plt.ylabel(r"$k_y$ (1/nm)")
-    plt.xlim(n//2-50,n//2+50)
-    plt.ylim(n//2-50,n//2+50)
-    plt.xticks(ticks=np.linspace(n//2-50, n//2+50, 5), labels=np.round(np.linspace(kx[n//2-50], kx[n//2+50], 5), 1))
-    plt.yticks(ticks=np.linspace(n//2-50, n//2+50, 5), labels=np.round(np.linspace(ky[n//2-50], ky[n//2+50], 5), 1))
+    plt.xlim(n//2-100,n//2+100)
+    plt.ylim(n//2-100,n//2+100)
+    plt.xticks(ticks=np.linspace(n//2-100, n//2+100, 5), labels=np.round(np.linspace(kx[n//2-100], kx[n//2+100], 5), 1))
+    plt.yticks(ticks=np.linspace(n//2-100, n//2+100, 5), labels=np.round(np.linspace(ky[n//2-100], ky[n//2+100], 5), 1))
     plt.colorbar(label='Intensity (AU)')
     plt.savefig(f"Produced_Plots/FFTSIM/HOPG_Lattice_Reciprocal_Space_{real_space_size}nm.png", dpi=300)
     plt.show()
@@ -86,8 +88,8 @@ def simulate_hopg_lattice(n_harmonics, k, theta, real_space_size,order,n):
 # Constants for HOPG lattice
 a = 0.246  # lattice constant for graphene in nm
 k = 2 * np.pi / a  # wave vector
-n_pts = 2000 # size of the simulation grid
+n_pts = 3000 # size of the simulation grid
 real_space_size = 3 # nm
 theta = np.pi / 3  
-n_harmonics = 20  # number of harmonics to include in the lattice
-simulate_hopg_lattice(n_harmonics, k, theta, real_space_size,5,n_pts)
+n_harmonics = 10  # number of harmonics to include in the lattice
+simulate_hopg_lattice(n_harmonics, k, theta, real_space_size,10,n_pts)
